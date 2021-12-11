86d810857680ca6c41541de1165ae24c8e2eb321
toast <58362380+traumatism@users.noreply.github.com>
2021-12-11 16:43:18 +0100

    Update add command

--- commands/add.py
+++ commands/add.py
@@ -10,13 +10,21 @@ class Add(CommandBase):
     description = "Insert data into the database"
 
     def init_arguments(self):
-        self.parser.add_argument("table", help="Table to add data to.", choices={table.name for table in database.tables})
-        self.parser.add_argument("data", help="Data to add.")
+        self.parser.add_argument(
+            "table", help="Table to add data to.", 
+            choices={table.name for table in database.tables}
+        )
+
+        self.parser.add_argument(
+            "data", help="Data to add."
+        )
 
     def execute(self):
         table = self.database.get_table_by_str(self.arguments.table)
 
         if table is False:
-            return Logger.warn(f"Table '{self.arguments.table}' does not exist.")
+            return Logger.warn(
+                f"Table '{self.arguments.table}' does not exist."
+            )
 
         self.database.insert_data(table(self.arguments.data))
